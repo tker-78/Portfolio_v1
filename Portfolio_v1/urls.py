7a3dfb2a817a3e6072b29266234b17895e24cfc0
tker-78 <kktak02@gmail.com>
2025-03-27 06:59:31 +0900

    changed project name

--- Portfolio_v1/urls.py
+++ Portfolio_v1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for UgokashiteDjango project.
+URL configuration for Portfolio_v1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- Portfolio_v1/wsgi.py
+++ Portfolio_v1/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for UgokashiteDjango project.
+WSGI config for Portfolio_v1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UgokashiteDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portfolio_v1.settings')
 
 application = get_wsgi_application()
