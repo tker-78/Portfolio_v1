6c0b3ec4da22b690b98f82e1486d390a12dfa64a
tker-78 <kktak02@gmail.com>
2025-03-30 11:44:34 +0900

    echartsのheatmapを使うことに変更

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -2,30 +2,81 @@ import * as Vue from "https://cdn.jsdelivr.net/npm/vue@3.2/dist/vue.esm-browser.
 
 const { createApp } = Vue
 
-export const calheatmap = createApp({
+export const heatmap = createApp({
     mounted() {
-        const cal = new CalHeatmap();
-        cal.paint(this.options);
+        const ctx1 = document.getElementById("heatmap-chart")
+        ctx1.width = ctx1.clientWidth * window.devicePixelRatio
+        ctx1.height = ctx1.clientHeight * window.devicePixelRatio
+        this.chart = echarts.init(ctx1)
+        this.getVirtualData('2025')
+        this.chart.setOption(this.options)
+    },
+    methods: {
+        getVirtualData(year) {
+              const date = +echarts.time.parse(year + '-01-01');
+              const end = +echarts.time.parse(+year + 1 + '-01-01');
+              const dayTime = 3600 * 24 * 1000;
+              const data = [];
+              for (let time = date; time < end; time += dayTime) {
+                data.push([
+                  echarts.time.format(time, '{yyyy}-{MM}-{dd}', false),
+                  Math.floor(Math.random() * 10000)
+                ]);
+              }
+              // this.options.series.data = data
+            this.options.series.data = [
+                ['2025-01-01', 1],
+                ['2025-01-02', 2],
+                ['2025-01-03', 3],
+            ]
+        }
     },
     data() {
         return {
+            data: null,
+            chart: null,
             options: {
-                range: 5,
-                domain: {type: 'month', sort: 'asc'},
-                subDomain: {type: 'day'},
-                scale: {
-                    color: {
-                        type: 'linear',
-                        range: ['#e6e6e6', '#4dd05a'],
-                        domain: [0, 1],
-                    },
+                title: {
+                top: 30,
+                left: 'center',
+                text: '日記投稿数'
+              },
+              tooltip: {},
+              visualMap: {
+                min: 0,
+                max: 10,
+                type: 'piecewise',
+                orient: 'horizontal',
+                left: 'center',
+                top: 65,
+                inRange: {
+                  // 緑系のグラデーション
+                  color: ['#e6f3e6', '#c6e6c6', '#a6d9a6', '#86cc86', '#66bf66', '#46b246', '#26a526', '#069906']
                 },
-            },
+              },
+              calendar: {
+                top: 120,
+                left: 30,
+                right: 30,
+                cellSize: ['auto', 18],
+                range: '2025',
+                itemStyle: {
+                  borderWidth: 0.5,
+                },
+                yearLabel: { show: false }
+              },
+              series: {
+                type: 'heatmap',
+                coordinateSystem: 'calendar',
+                data: null
+              }
+            }
         }
     }
+
 })
 
-calheatmap.mount('#cal-heatmap')
+heatmap.mount('#heatmap')
 
 
 
